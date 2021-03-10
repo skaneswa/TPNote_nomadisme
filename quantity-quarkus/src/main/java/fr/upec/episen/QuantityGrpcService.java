171c16a28fb88241ff7587c21dedcbe4d782a1e6
Sakana <sakana.kaneswaran@gmail.com>
2021-03-10 19:34:28 +0100

    Exercice 2

---
+++ quantity-quarkus/src/main/java/fr/upec/episen/QuantityGrpcService.java
@@ -0,0 +1,42 @@
+package fr.upec.episen;
+
+import fr.upec.episen.proto.QuantityGrpc;
+import fr.upec.episen.proto.QuantityNameReply;
+import fr.upec.episen.proto.QuantityNameRequest;
+import io.grpc.stub.StreamObserver;
+
+import javax.inject.Singleton;
+
+
+@Singleton
+public class QuantityGrpcService extends QuantityGrpc.QuantityImplBase {
+
+    @Override
+    public void getQuantity(QuantityNameRequest request, StreamObserver<QuantityNameReply> responseObserver) {
+        String isbn = request.getIsbn();
+        int quantity ;
+
+        switch (isbn){
+            case "978-2070541270":
+                quantity = 9;
+                break;
+            case "978-2070541271":
+                quantity = 12;
+                break;
+            case "978-2070541272":
+                quantity = 5;
+                break;
+            default:
+                quantity = -1;
+                break;
+        }
+
+        responseObserver.onNext(QuantityNameReply
+            .newBuilder()
+                .setQuantity(quantity)
+                .build()
+        );
+
+        responseObserver.onCompleted();
+    }
+}
